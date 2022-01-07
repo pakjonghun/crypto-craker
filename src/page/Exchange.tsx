99d9214abe378faf12a2d098d56f89433f618a3e
정훈박 <fireking5997@gmail.com>
2022-01-07 21:22:50 +0900

    Fix:CI=false 설정

--- src/page/Exchange.tsx
+++ src/page/Exchange.tsx
@@ -3,23 +3,6 @@ import { useOutletContext } from "react-router-dom";
 import { useQuery } from "react-query";
 import { getExchange } from "../api";
 
-const init = {
-  options: {
-    chart: {
-      id: "basic-bar",
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-    },
-  },
-  series: [
-    {
-      name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
-    },
-  ],
-};
-
 type TypeExchang = {
   exchange_id: string;
   exchange_name: string;
